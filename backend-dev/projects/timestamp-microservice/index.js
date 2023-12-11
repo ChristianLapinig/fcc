01523e637871bdc901af76f86e66e12c511a85ed
Christian Lapinig <clapinig@adobe.com>
2023-12-11 09:18:10 -0800

    Initial index.js file

---
+++ backend-dev/projects/timestamp-microservice/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    name: "FCC Timestamp Microservice",
+    description:
+      "A microservice that returns a JSON object with a unix timestamp and a natural language date.",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server now listening on port ${PORT}`);
+});
