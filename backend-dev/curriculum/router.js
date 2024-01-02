d800534b9e118d991a47ba7ff6f12ad1b6cedcbb
Christian Lapinig <clapinig@adobe.com>
2024-01-02 13:24:09 -0800

    Add a chain middleware endpoint

--- backend-dev/curriculum/router.js
+++ backend-dev/curriculum/router.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-// Route for "Start a Working Express Server"
-router.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
+const MESSAGE = "Hello json";
+
+// Serve JSON to a specific route
+router.get("/hello/json", (req, res) => {
+  res.json({
+    message: MESSAGE_STYLE == "uppercase" ? MESSAGE.toUpperCase() : MESSAGE,
+  });
+});
+
+// Chain Middleware to Create a Time Server
+router.get("/chain-middleware/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({
+    time: req.time,
+  });
 });
 
 module.exports = router;
