c1f1725f2fbf05392ae89f929e8fcfd28ca3e339
Christian Lapinig <clapinig@adobe.com>
2024-01-14 02:23:26 -0800

    Merge with chain middleware branch

--- backend-dev/curriculum/router.js
+++ backend-dev/curriculum/router.js
@@ -6,7 +6,6 @@ const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
 const MESSAGE = "Hello json";
 const router = express.Router();
 
-
 // Serve an HTML file 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../views/index.html"));
@@ -18,6 +17,14 @@ Use the .env File
 */
 router.get("/hello/json", (req, res) => {
   res.json({ message: MESSAGE_STYLE === "uppercase" ? MESSAGE.toUpperCase() : MESSAGE });
+});
+
+// Chain Middleware to Create a Time Server
+router.get("/chain-middleware/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
 });
 
 module.exports = router;
